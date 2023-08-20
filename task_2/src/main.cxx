020de3071e872a687c145543009c0abf4e6d3c9a
JateX <grisaia@inbox.ru>
2023-08-20 14:47:20 +0300

    Maybe fix 2

--- task_2/src/main.cxx
+++ task_2/src/main.cxx
@@ -1,39 +1,52 @@
 #include <iostream>
+#include <future>
+#include <algorithm>
 #include <random>
-#include <thread>
 #include <vector>
 
 template<class InputIterator, class Function>
 void parallel_for_each(InputIterator begin, InputIterator end, Function f) {
-    auto range = std::distance(begin, end);
-    size_t numThreads = std::thread::hardware_concurrency();
- 
-    // number of elements for one thread
-    size_t jobsForThread = range / numThreads;
-    // number of elements for one thread + remainder
-    size_t jobsForMainThread = range % numThreads + jobsForThread;
- 
-    // minus main thread
-    std::vector<std::thread> threads(numThreads - 1);
- 
-    InputIterator block_start = begin;
-    InputIterator block_end = begin + jobsForThread;
- 
-    for(size_t i = 0; i < numThreads - 1; ++i) {
-        threads[i] = std::thread(std::for_each<InputIterator, Function>, block_start, block_end, f);
+    size_t range = std::distance(begin, end);
+    size_t max_chunk = 50;
+    if (range <= max_chunk)
+        std::for_each(begin, end, f);
+    else {
+        InputIterator block_start = begin;
+        InputIterator block_end = begin + max_chunk;
+        auto res = std::async(std::launch::async, parallel_for_each<InputIterator, Function>, block_start, block_end, f);
         block_start = block_end;
-        block_end += jobsForThread;
+        parallel_for_each(block_start, end, f);
     }
 
-    std::for_each(block_start, block_start + jobsForMainThread, f);
- 
-    for(auto& t : threads) {
-            t.join();
-    }
+
+    //size_t numThreads = std::thread::hardware_concurrency();
+    //
+    //// number of elements for one thread
+    //size_t jobsForThread = range / numThreads;
+    //// number of elements for one thread + remainder
+    //size_t jobsForMainThread = range % numThreads + jobsForThread;
+    //
+    //// minus main thread
+    //std::vector<std::thread> threads(numThreads - 1);
+    //
+    //InputIterator block_start = begin;
+    //InputIterator block_end = begin + jobsForThread;
+    //
+    //for(size_t i = 0; i < numThreads - 1; ++i) {
+    //    threads[i] = std::thread(std::for_each<InputIterator, Function>, block_start, block_end, f);
+    //    block_start = block_end;
+    //    block_end += jobsForThread;
+    //}
+    //
+    //std::for_each(block_start, block_start + jobsForMainThread, f);
+    //
+    //for(auto& t : threads) {
+    //        t.join();
+    //}
 }
 
-void add_data(std::vector<int>& otherVector) {
-    for (int i=0; i < 20; ++i) {
+void add_data(std::vector<int>& otherVector, int n_ = 50) {
+    for (int i=0; i < n_; ++i) {
         std::random_device rd;
 	    std::mt19937 gen{ rd() };
 	    std::uniform_int_distribution<> dis(0, 100);
@@ -46,9 +59,11 @@ void multiply(int& n) { n *= 2; }
 
 int main(void) {
     std::vector<int> data;
-    add_data(data);
+    int nNumbers = 200;
+    add_data(data, nNumbers);
 
     // Before
+    std::cout << "Before:\n";
     for (auto const &v : data)
         std::cout << " " << v;
     std::cout << std::endl;
@@ -57,9 +72,12 @@ int main(void) {
     parallel_for_each(data.begin(), data.end(), multiply);
 
     // After
+    std::cout << "After:\n";
     for (auto const &v : data)
         std::cout << " " << v;
     std::cout << std::endl;
+
+    getchar();
 
 
     return EXIT_SUCCESS;
